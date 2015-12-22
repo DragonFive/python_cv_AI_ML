7a117873663d8e226ee6d7b95ad87dc3edb6d984
DragonFive <dragonfive2013@163.com>
2015-12-23 00:40:01 +0800

    添加自己写的库mytools

---
+++ myTools.py
@@ -0,0 +1,75 @@
+# -*- coding: gbk -*-
+#__author__ = 'ASUS'
+
+import time,sys, urllib, urllib2, json,socket,re
+
+# 新建一个python文件
+def newPy(fname):
+    fileName=r"E:\资料\onedrive\code\python\opencv"+"\\"+fname;
+    f = open(fileName, "a");
+    f.write("# -*- coding: gbk -*-\n#__author__ = 'dragonfive'\n");
+    # 获取当前时间
+    now_clock = getSysTime()
+    f.write("# "+now_clock+"\n")
+    # 获取当前地点
+    myip = Getmyip()
+    address = myip.getAddByIp(myip.get_ex_ip()).encode('gbk');
+    f.write("# "+address+" dragonfive \n");
+    # 版权信息
+    f.write("# copyright 1992-"+time.strftime(r"%Y")+ " dragonfive \n");
+    f.close();
+
+# 获取系统时间
+def getSysTime():
+    now_clock = time.strftime(r"%d/%m/%Y %H:%M:%S")
+    return now_clock
+
+# 获取百度ipstore的apikey
+def getMyApikey():
+    return "9b447786edf207a8813e115d44d85187"
+
+
+class Getmyip:
+    # 获得本机内网IP
+    def get_local_ip(self):
+        localIP=socket.gethostbyname(socket.gethostname())
+        return localIP;
+
+    # 获取外网IP
+    def get_ex_ip(self):
+        try:
+            myip = self.visit("http://www.whereismyip.com/")
+            return myip
+        except:
+            try:
+                myip = self.visit("http://www.bliao.com/ip.phtml")
+            except:
+                try:
+                    myip = self.visit("http://www.ip138.com/ip2city.asp")
+                except:
+                    myip = "So sorry!!!"
+                    return myip
+
+    def visit(self,url):
+        opener = urllib2.urlopen(url)
+        ourl=opener.geturl()
+        if url == ourl:
+            str = opener.read()
+            asd=re.search('\d+\.\d+\.\d+\.\d+',str).group(0)
+            return asd
+
+    # 获取一个ip的区域地址，用百度ipstore的工具;
+    def getAddByIp(self,ipaddr):#这里可以用多参数进行优化；
+        url = 'http://apis.baidu.com/chazhao/ipsearch/ipsearch?ip='+ipaddr
+        req = urllib2.Request(url)
+
+        req.add_header("apikey", getMyApikey());
+
+        resp = urllib2.urlopen(req)
+        content = json.loads(resp.read())
+        if(content):
+            address=content["data"]["country"]+":"+content["data"]["city"]+":"+content["data"]["operator"]
+            return address;
+
+
+
---
+++ testNew.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#__author__ = 'dragon'
+
+from myTools import Getmyip,newPy
+import random
+
+# newPy("07gauss2dim.py");
+# print(Getmyip().getip());
+# print(getAddByIp(getExNetIp()))
+# Sigma = 1;
+# mu1 = 0;
+# a=random.gauss(Sigma,mu1)
+# print(a);
+
+a=[1,2,3,4,5]
+print([i for i in a if i>2])
+
