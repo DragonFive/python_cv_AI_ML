c3fd85195e1ec90f59b46876e5782afb71bb4e96
DragonFive <1690302963@qq.com>
2017-01-10 21:50:21 +0800

    加入结构化特征，纹理特征

--- cv_python/imageSearch/index.py
+++ cv_python/imageSearch/index.py
@@ -1,35 +1,41 @@
-from colordescriptor import ColorDescriptor
-import argparse
+import colordescriptor
+import structuredescriptor
 import glob
+import argparse
 import cv2
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-d", "--dataset", required = True,
-	help = "Path to the directory that contains the images to be indexed")
-ap.add_argument("-i", "--index", required = True,
-	help = "Path to where the computed index will be stored")
-args = vars(ap.parse_args())
- 
-# initialize the color descriptor
-cd = ColorDescriptor((8, 12, 3))
+searchArgParser = argparse.ArgumentParser()
+searchArgParser.add_argument("-d", "--dataset", required = True, help = "Path to the directory that contains the images to be indexed")
+searchArgParser.add_argument("-c", "--colorindex", required = True, help = "Path to where the computed color index will be stored")
+searchArgParser.add_argument("-s", "--structureindex", required = True, help = "Path to where the computed structure index will be stored")
+arguments = vars(searchArgParser.parse_args())
 
-# open the output index file for writing
-output = open(args["index"], "w")
- 
-# use glob to grab the image paths and loop over them
-for imagePath in glob.glob(args["dataset"] + "/*.png"):
-	# extract the image ID (i.e. the unique filename) from the image
-	# path and load the image itself
-	imageID = imagePath[imagePath.rfind("/") + 1:]
-	image = cv2.imread(imagePath)
- 
-	# describe the image
-	features = cd.describe(image)
- 
-	# write the features to file
-	features = [str(f) for f in features]
-	output.write("%s,%s\n" % (imageID, ",".join(features)))
- 
-# close the index file
+idealBins = (8, 12, 3)
+colorDesriptor = colordescriptor.ColorDescriptor(idealBins)
+
+output = open(arguments["colorindex"], "w")
+
+for imagePath in glob.glob(arguments["dataset"] + "/*.png"):
+    imageName = imagePath[imagePath.rfind("/") + 1 : ]
+    image = cv2.imread(imagePath)
+    features = colorDesriptor.describe(image)
+    # write features to file
+    features = [str(feature).replace("\n", "") for feature in features]
+    output.write("%s,%s\n" % (imageName, ",".join(features)))
+# close index file
+output.close()
+
+idealDimension = (16, 16)
+structureDescriptor = structuredescriptor.StructureDescriptor(idealDimension)
+
+output = open(arguments["structureindex"], "w")
+
+for imagePath in glob.glob("dataset" + "/*.png"):
+    imageName = imagePath[imagePath.rfind("/") + 1 : ]
+    image = cv2.imread(imagePath)
+    structures = structureDescriptor.describe(image)
+    # write structures to file
+    structures = [str(structure).replace("\n", "") for structure in structures]
+    output.write("%s,%s\n" % (imageName, ",".join(structures)))
+# close index file
 output.close()
