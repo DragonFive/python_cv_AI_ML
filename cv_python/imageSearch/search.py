0452b7f8ffd72c67a2956867a0abe57149fd9e3f
DragonFive <1690302963@qq.com>
2017-01-10 21:51:09 +0800

    加入对结构化特征的解析

--- cv_python/imageSearch/search.py
+++ cv_python/imageSearch/search.py
@@ -1,38 +1,38 @@
-from colordescriptor import ColorDescriptor
-from searcher import Search
+import colordescriptor 
+import structuredescriptor
+import searcher
 import argparse
 import cv2
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--index", required = True,
-	help = "Path to where the computed index will be stored")
-ap.add_argument("-q", "--query", required = True,
-	help = "Path to the query image")
-ap.add_argument("-r", "--result-path", required = True,
-	help = "Path to the result path")
-args = vars(ap.parse_args())
- 
-# initialize the image descriptor
-cd = ColorDescriptor((8, 12, 3))
+searchArgParser = argparse.ArgumentParser()
+searchArgParser.add_argument("-c", "--colorindex", required = True, help = "Path to where the computed color index will be stored")
+searchArgParser.add_argument("-s", "--structureindex", required = True, help = "Path to where the computed structure index will be stored")
+searchArgParser.add_argument("-q", "--query", required = True, help = "Path to the query image")
+searchArgParser.add_argument("-r", "--resultpath", required = True, help = "Path to the result path")
+searchArguments = vars(searchArgParser.parse_args())
 
+idealBins = (8, 12, 3)
+idealDimension = (16, 16)
 
-# load the query image and describe it
-query = cv2.imread(args["query"])
-features = cd.describe(query)
- 
-# perform the search
-searcher = Searcher(args["index"])
-results = searcher.search(features)
- 
-# display the query
-cv2.imshow("Query", query)
- 
-# loop over the results
-for (score, resultID) in results:
-	# load the result image and display it
-	result = cv2.imread(args["result_path"] + "/" + resultID)
-	cv2.imshow("Result", result)
-	cv2.waitKey(0)
+colorDescriptor = colordescriptor.ColorDescriptor(idealBins)
+structureDescriptor = structuredescriptor.StructureDescriptor(idealDimension)
+queryImage = cv2.imread(searchArguments["query"])
+colorIndexPath = searchArguments["colorindex"]
+structureIndexPath = searchArguments["structureindex"]
+resultPath = searchArguments["resultpath"]
+
+queryFeatures = colorDescriptor.describe(queryImage)
+queryStructures = structureDescriptor.describe(queryImage)
+
+imageSearcher = searcher.Searcher(colorIndexPath, structureIndexPath)
+searchResults = imageSearcher.search(queryFeatures, queryStructures,5)
+
+for imageName, score in searchResults:
+    queryResult = cv2.imread(resultPath + "/" + imageName)
+    cv2.imshow("Result Score: " + str(int(score)) + " (lower is better)", queryResult)
+    cv2.waitKey(0)
+
+cv2.imshow("Query", queryImage)
+cv2.waitKey(0)
 
 
--- cv_python/imageSearch/searcher.py
+++ cv_python/imageSearch/searcher.py
@@ -1,13 +1,24 @@
+# -*- coding: utf-8 -*-
 # import the necessary packages
 import numpy as np
 import csv
+import re
  
 class Searcher:
-	def __init__(self, indexPath):
+	def __init__(self, colorIndexPath, structureIndexPath):
 		# store our index path
-		self.indexPath = indexPath
- 
-	def search(self, queryFeatures, limit = 10):
+		self.indexPath, self.structureIndexPath = colorIndexPath, structureIndexPath
+
+	def chi2_distance(self, histA, histB, eps = 1e-10):
+		# compute the chi-squared distance
+		d = 0.5 * np.sum([((a - b) ** 2) / (a + b + eps)
+		    for (a, b) in zip(histA, histB)])
+
+		# return the chi-squared distance
+		return d
+	
+	# 这里的返回值是一个字典，内容是图像名和特征的对应;
+	def searchByColor(self, queryFeatures):
 		# initialize our dictionary of results
 		results = {}
 
@@ -33,18 +44,64 @@ class Searcher:
  
 			# close the reader
 			f.close()
- 
+ 		return results
+		# 这里还不需要排序
 		# sort our results, so that the smaller distances (i.e. the
 		# more relevant images are at the front of the list)
-		results = sorted([(v, k) for (k, v) in results.items()])
+		#results = sorted([(v, k) for (k, v) in results.items()])
  
 		# return our (limited) results
-		return results[:limit]
+		#return results[:limit]
 
-def chi2_distance(self, histA, histB, eps = 1e-10):
-    # compute the chi-squared distance
-    d = 0.5 * np.sum([((a - b) ** 2) / (a + b + eps)
-        for (a, b) in zip(histA, histB)])
+	def structureDistance(self, structures, queryStructures, eps = 1e-5):
+		distance = 0
+		normalizeRatio = 5e3
+		for index in xrange(len(queryStructures)):
+			for subIndex in xrange(len(queryStructures[index])):
+				a = structures[index][subIndex]
+				b = queryStructures[index][subIndex]
+				distance += (a - b) ** 2 / (a + b + eps)
+		return distance / normalizeRatio
+    
+    # 把矩阵转化为向量;
+	def transformRawQuery(self, rawQueryStructures):
+		queryStructures = []
+		for substructure in rawQueryStructures:
+			structure = []
+			for line in substructure:
+				for tripleColor in line:
+					structure.append(float(tripleColor))
+			queryStructures.append(structure)
+		return queryStructures
+	    
+	def searchByStructure(self, rawQueryStructures):
+		searchResults = {}
+		queryStructures = self.transformRawQuery(rawQueryStructures)
+		with open(self.structureIndexPath) as indexFile:
+			reader = csv.reader(indexFile)
+			for line in reader:
+				structures = []
+				for structure in line[1:]:
+					structure = structure.replace("[", "").replace("]", "")
+					structure = re.split("\s+", structure)
+					if structure[0] == "":
+						structure = structure[1:]
+					structure = [float(eachValue) for eachValue in structure]
+					structures.append(structure)
+				distance = self.structureDistance(structures, queryStructures)
+				searchResults[line[0]] = distance
+			indexFile.close()
+		# print "structure", sorted(searchResults.iteritems(), key = lambda item: item[1], reverse = False)
+		return searchResults
 
-    # return the chi-squared distance
-    return d
+
+	
+	def search(self, queryFeatures, rawQueryStructures, limit = 3):
+		featureResults = self.searchByColor(queryFeatures)
+		structureResults = self.searchByStructure(rawQueryStructures)
+		results = {}
+		for key, value in featureResults.iteritems():
+			results[key] = value + structureResults[key]
+		results = sorted(results.iteritems(), key = lambda item: item[1], reverse = False)
+		return results[ : limit]
+
